bf09f35f29c26a64c9152c2a35dc25237ecf005b
Lijin <lijin@baabte.com>
2015-06-18 10:08:57 +0530

    App for candidate

---
+++ angularModules/candidateCourseRelated/candidateCourseView/partials/Partial-candidateCourseView.js
@@ -0,0 +1,77 @@
+angular.module('baabtra').controller('viewCandidateCourseCtrl',
+	['commonService','$scope','$state','$rootScope','candidateCourseFullView',
+	function(commonService,$scope,$state,$rootScope,CCFV){
+
+		 if(!$rootScope.userinfo){
+		   commonService.GetUserCredentials($scope);
+		   $rootScope.hide_when_root_empty=false;
+		 }
+		 
+		 if(angular.equals($rootScope.loggedIn,false)){
+		   $state.go('login');
+		 }
+
+		 /*login detils ends*/
+
+		 $scope.lastViewedOrder=0;
+		 $scope.lastElement=true;
+		 $scope.totalMark=0;
+		 $scope.markScored=0;
+
+		 $scope.getElementClicked = false;
+
+		 $scope.getElement=function (direction) {
+		 	$scope.getElementClicked = true;
+		 	var gotCurrentElement=CCFV.getCurrentElement(userLoginId,courseMappingId,direction);
+		    gotCurrentElement.then(function (data) {
+		    	
+		    	var dataElement=angular.fromJson(JSON.parse(data.data));
+		    	console.log(dataElement);
+		    	if(dataElement=="error"){
+					$state.go('home.main.CandidateCourseView');
+		    	}
+		    	if(dataElement.tlPoint==0){
+		    		$scope.tlPointNam='';
+		    	}
+		    	else{
+		    		var selectedDuration=$scope.ddlBindObject[dataElement.selectedDuration-1];
+			    	var tlPointName=selectedDuration.name.replace('(s)',' ');
+			    	$scope.tlPointName=tlPointName+(Math.ceil(dataElement.tlPoint*selectedDuration.mFactor));
+		    	}
+
+		    	//Anoop: pushing the course assigned date into the prevObj
+		    	$scope.PrevObj=dataElement.element;
+		    	$scope.PrevObj.courseAssignedDate = dataElement.courseAssignedDate;
+
+		    	$scope.courseId=dataElement.courseId;
+		    	$scope.lastViewedOrder=dataElement.lastViewedOrder;
+		 		$scope.lastElement=dataElement.lastElement;
+		 		$scope.totalMark=dataElement.totalMark;
+				$scope.markScored=dataElement.markScored;
+				$scope.getElementClicked = false;
+
+		    });
+		 };
+
+		 $scope.navigateToTimeline=function(courseId){
+				$state.go('home.main.viewCourse',{courseId:courseId.$oid});
+			};
+
+		 $scope.navigateToSummary=function(courseId){
+				$state.go('home.main.course',{courseId:courseId.$oid});
+			};
+
+		$scope.ddlBindObject = {0:{id: "1",name: "Year(s)",mFactor:(1/525600),show:true},
+                        1:{id: "2",name: "Month(s)",mFactor:(1/43200),show:true},
+                        2:{id: "3",name: "Week(s)",mFactor:(1/10080),show:true},
+                        3:{id: "4",name: "Day(s)",mFactor:(1/1440),show:true},
+                        4:{id: "5",name: "Hour(s)",mFactor:1/60,show:true},
+                        5:{id: "6",name: "Minute(s)",mFactor:1,show:true}};//mFactor is multiplication factor
+
+		var userLoginId=$rootScope.userinfo.userLoginId;
+		var courseMappingId=$state.params.courseMappingId;
+		$scope.getElement('');
+
+
+
+	}]);
---
+++ angularModules/candidateCourseRelated/courseElementFullView/directives/Directive-courseElementFullView.js
@@ -0,0 +1,101 @@
+angular.module('baabtra').directive('courseElementFullView',['$compile', function($compile) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			previewData:"=",
+			tlPosition:"@",
+			courseId:'='
+		},
+		templateUrl: 'angularModules/candidateCourseRelated/courseElementFullView/directives/Directive-courseElementFullView.html',
+		link: function(scope, element, attrs, fn) {
+			scope.rand=Math.floor(Math.random()*100000); // for generating random id for elements
+			//alternative elements
+			var secondaryElements={
+				"doc-viewer":'doc-viewer-frame'
+			};
+	
+		scope.$watch('previewData', function(){
+
+			
+
+			scope.childElementScopes = {};
+				$(element).find('#elementContent'+scope.rand).html('');
+				if(!angular.equals(scope.previewData,undefined)){
+					
+					angular.forEach(scope.previewData.elements, function(data,key){//looping through each type of course elements at this point in the object
+							if(data instanceof Object){
+
+								if(angular.equals(data.parentElementId, undefined)){
+
+									if(angular.isDefined(data.uniqueId)){
+										var subArray = returnSub(scope.previewData.elements, data.uniqueId);
+
+										if(subArray.length){
+											data.subElems = subArray;
+										}
+									}
+
+									var elemDirName=data.type;
+									if(secondaryElements[elemDirName]){
+										//checks for alternative viewer
+										elemDirName=secondaryElements[elemDirName];
+									}
+							 		var elementToBeCreated = $('<'+elemDirName+'>');							 		
+							 		
+							 		
+							 		//checking for custom attributes and adding them
+							 		if(!angular.equals(data.customAttributes, undefined)) {
+							 			
+							 			for (var keyAttrib in data.customAttributes){
+							 				elementToBeCreated.attr(keyAttrib,data.customAttributes[keyAttrib]);
+							 			}
+							 		}
+
+							 		elementToBeCreated.attr('data',JSON.stringify(data));
+							 		elementToBeCreated.attr('course-element',JSON.stringify(scope.previewData));
+							 		elementToBeCreated.attr('index',key);
+							 		elementToBeCreated.attr('this-scope','childElementScopes.'+key+'');
+							 		elementToBeCreated.attr('course-id','courseId');
+							 		elementToBeCreated.addClass('elementField m-v-lg');
+
+							 		
+							 		$('#elementContent'+scope.rand).append(elementToBeCreated);
+								}
+							}
+					});
+					if(!angular.equals(scope.previewData.nestedElements,undefined)){
+						// if it is having any nested elements we have to append like this
+						// then only it can append same directive inside this directive
+						var elem='<nested-element data="previewData.nestedElements"></nested-element>';
+						$('#elementContent'+scope.rand).append(elem);
+
+					} 
+						
+				}
+				// now we have to compile the view to render all the directives that we have added manually using js
+				$compile(element)(scope);
+			},true);
+
+			//a function to return the sub elements of a parent element
+			function returnSub(inputArray, parentId){
+				// initialise an array to hold the sub elements
+				var subArray = [];
+				var currentElement = {};
+
+				for (var i in inputArray){
+					currentElement = inputArray[i];
+
+					if(angular.isDefined(currentElement.parentElementId) && angular.equals(currentElement.parentElementId,parentId)){
+						subArray.push(currentElement);
+					}
+				}
+
+				return subArray;
+			}
+			// *************************************************************************************
+
+
+		}
+	};
+}]);
